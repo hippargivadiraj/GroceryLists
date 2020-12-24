3959d84e8f5c18620c78b37062b1b6a42f52fdaf
Vadiraj Hippargi <Vadi@leadconsultant.com>
2020-12-23 23:28:36 -0800

    Initial Commit

---
+++ GroceryLists/GroceryListsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryListsApp.swift
+//  GroceryLists
+//
+//  Created by Vadiraj Hippargi on 12/23/20.
+//
+
+import SwiftUI
+
+@main
+struct GroceryListsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
